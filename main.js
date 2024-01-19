dcdeaa9b4ac1fb9ca7e1c0140ee4a3ca9047a552
agnesvarga1 <agikolyok@gmail.com>
2024-01-19 14:24:28 +0100

    fix  digits display

--- main.js
+++ main.js
@@ -2,21 +2,22 @@ const secHtml = document.querySelector("#sec");
 const minsHtml = document.querySelector("#mins");
 const hrsHtml = document.querySelector("#hrs");
 const timerDisp = document.querySelector(".timer");
-let endDate = new Date("January 19, 2024 09:30:00");
+let endDate = new Date("January 20, 2024 09:30:00");
 let endDateInMs = endDate.getTime();
-
-const secondInMs = 1000;
-const minuteInMs = 60 * secondInMs;
-const hourInMs = 60 * minuteInMs;
-const dayInMs = 24 * hourInMs;
 
 function timer() {
   let nowInMs = new Date().getTime();
   let diff = endDateInMs - nowInMs;
+  let diffInS = diff / 1000;
+  let mTotal = Math.floor(diffInS / 60);
+  let h = Math.floor(mTotal / 60);
+  let m = Math.floor(mTotal - h * 60);
+  let s = Math.floor(diffInS - mTotal * 60);
+
   if (diff > 0) {
-    hrsHtml.innerHTML = Math.floor((diff % dayInMs) / hourInMs);
-    minsHtml.innerHTML = Math.floor((diff % hourInMs) / minuteInMs);
-    secHtml.innerHTML = Math.floor((diff % minuteInMs) / secondInMs);
+    h >= 10 ? (hrsHtml.innerHTML = h) : (hrsHtml.innerHTML = `0${h}`);
+    m >= 10 ? (minsHtml.innerHTML = m) : (minsHtml.innerHTML = `0${m}`);
+    s >= 10 ? (secHtml.innerText = s) : (secHtml.innerText = `0${s}`);
   } else {
     clearInterval(countDown);
     timerDisp.innerHTML = "Evviva inizia la lezione";
